8f289cba27c4c7a977f5ec1e6a8d30c430423d04
Alex Ford <Alex@CodeTunnel.com>
2013-02-05 09:56:39 -0700

    Removed code to set default font-size per element.

--- jquery.dynamitext.js
+++ jquery.dynamitext.js
@@ -39,11 +39,6 @@
         if (settings.startingSizeIsBaseSize) {
             settings.baseWidth = settings.$resizingElement.width();
             settings.baseHeight = settings.$resizingElement.height();
-
-            $elems.each(function () {
-                var baseFontSize = parseInt(settings.$resizingElement.css('font-size'));
-                $(this).data('baseFontSize', baseFontSize);
-            });
         }
 
         $elems.css('overflow', 'hidden');
